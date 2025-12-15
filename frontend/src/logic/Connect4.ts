f02815ae0c8f6687b48c192774442afa24a89c2b
Arnauw <arnaud.rabel@gmail.com>
2025-12-15 11:56:38 +0100

    need to finish the pseudo logic for the game

--- frontend/src/logic/Connect4.ts
+++ frontend/src/logic/Connect4.ts
@@ -1,5 +1,6 @@
 export type Player = 1 | 2;
 export type Cell = Player | 0;
+export type ColumnIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
 export class Connect4 {
     rows = 6;
@@ -14,7 +15,7 @@ export class Connect4 {
         this.currentPlayer = 1;
         this.winner = null;
         this.gameOver = false;
-    }
+    };
     
     private createGrid(): Cell[][] {
         const board: Cell[][] = [];
@@ -26,9 +27,26 @@ export class Connect4 {
             board.push(row);
         }
         return board;
-    }
+    };
+
+    private switchTurn(): Player {
+        this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
+        return this.currentPlayer;
+    };
+
+    private dropPiece(column: ColumnIndex) {
+        // check if column is either full or if input is valid
+        if (column < 0 && column > 6) {
+            return false;
+        }
+        // if there is room on the column to actually drop the piece
+
+
+
+        console.log(column);
+    };
     
     printBoard() {
         console.table(this.board);
-    }
+    };
 }
