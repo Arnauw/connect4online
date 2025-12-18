c0985b32bdcb89ed8cbf314c0fc90ff887204b7e
Arnauw <arnaud.rabel@gmail.com>
2025-12-18 10:03:58 +0100

    fix test favicon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import {GameBoard} from "./components/GameBoard.tsx";
-import logo from './assets/imgs/logo.png';
+// import logo from './assets/imgs/logo.png';
 
 function App() {
 
   return (
     <>
-        <img className={"App-logo"} src={logo} alt={"test-image"}/>
+        {/*<img className={"App-logo"} src={logo} alt={"test-image"}/>*/}
         <GameBoard/>
     </>
   )
