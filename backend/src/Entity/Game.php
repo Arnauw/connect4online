9689bd8e01452dfefa999239f82e0e5aab4ef79a
Arnauw <arnaud.rabel@gmail.com>
2026-03-08 10:28:49 +0700

    added games entity + migration

---
+++ backend/src/Entity/Game.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GameRepository::class)]
+class Game
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'games')]
+    private ?User $player1 = null;
+
+    #[ORM\ManyToOne]
+    private ?User $player2 = null;
+
+    #[ORM\ManyToOne]
+    private ?User $winner = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?array $board = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $currentTurn = null;
+
+    #[ORM\Column(length: 20, nullable: true)]
+    private ?string $status = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $roomCode = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlayer1(): ?User
+    {
+        return $this->player1;
+    }
+
+    public function setPlayer1(?User $player1): static
+    {
+        $this->player1 = $player1;
+
+        return $this;
+    }
+
+    public function getPlayer2(): ?User
+    {
+        return $this->player2;
+    }
+
+    public function setPlayer2(?User $player2): static
+    {
+        $this->player2 = $player2;
+
+        return $this;
+    }
+
+    public function getWinner(): ?User
+    {
+        return $this->winner;
+    }
+
+    public function setWinner(?User $winner): static
+    {
+        $this->winner = $winner;
+
+        return $this;
+    }
+
+    public function getBoard(): ?array
+    {
+        return $this->board;
+    }
+
+    public function setBoard(?array $board): static
+    {
+        $this->board = $board;
+
+        return $this;
+    }
+
+    public function getCurrentTurn(): ?int
+    {
+        return $this->currentTurn;
+    }
+
+    public function setCurrentTurn(?int $currentTurn): static
+    {
+        $this->currentTurn = $currentTurn;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getRoomCode(): ?string
+    {
+        return $this->roomCode;
+    }
+
+    public function setRoomCode(?string $roomCode): static
+    {
+        $this->roomCode = $roomCode;
+
+        return $this;
+    }
+}
--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -26,6 +28,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
             'volume' => 50,
         ];
         $this->isVerified = false;
+        $this->games = new ArrayCollection();
     }
 
     #[ORM\Id]
@@ -68,6 +71,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $updatedAt = null;
+
+    /**
+     * @var Collection<int, Game>
+     */
+    #[ORM\OneToMany(targetEntity: Game::class, mappedBy: 'player1')]
+    private Collection $games;
 
     public function getId(): ?int
     {
@@ -228,5 +237,35 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setUpdatedAt(?\DateTimeImmutable $updatedAt): void
     {
         $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return Collection<int, Game>
+     */
+    public function getGames(): Collection
+    {
+        return $this->games;
+    }
+
+    public function addGame(Game $game): static
+    {
+        if (!$this->games->contains($game)) {
+            $this->games->add($game);
+            $game->setPlayer1($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGame(Game $game): static
+    {
+        if ($this->games->removeElement($game)) {
+            // set the owning side to null (unless already changed)
+            if ($game->getPlayer1() === $this) {
+                $game->setPlayer1(null);
+            }
+        }
+
+        return $this;
     }
 }
