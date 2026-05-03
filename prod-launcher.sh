77009f7ec8175ae180c2de0c96fa17ecb0a996f3
Arnauw <arnaud.rabel@gmail.com>
2026-05-03 19:17:30 +0200

    adds a script to execute on prod server after a reboot of the server to start docker containers and mail workers, I will find a better solution with services and cron later

---
+++ prod-launcher.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo "Starting Docker containers..."
+cd /var/www/mywebapps/connect4online/backend
+docker compose --env-file .env.local up -d
+
+echo "Waiting for database to be ready..."
+sleep 5
+
+echo "Stopping any existing Messenger workers..."
+php bin/console messenger:stop-workers --env=prod 2>/dev/null || true
+
+echo "Starting Messenger worker..."
+nohup php bin/console messenger:consume async --env=prod -vv >> var/log/messenger-worker.log 2>&1 &
+
+echo "Done. Worker PID: $!"
